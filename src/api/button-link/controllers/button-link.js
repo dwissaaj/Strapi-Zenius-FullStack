09894582dc594daa3c49ba8f59174f7e4b469c2b
dwisaji <dwis.buntelman123@gmail.com>
2022-06-08 21:20:48 +0700

    add api

---
+++ src/api/button-link/controllers/button-link.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  button-link controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::button-link.button-link');
---
+++ src/api/button-link/services/button-link.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * button-link service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::button-link.button-link');
